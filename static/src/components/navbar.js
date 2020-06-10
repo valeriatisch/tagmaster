0712195c767b1da69e42ca0e04f11e2e38f2feda
Nilinzm <64643592+Nilinzm@users.noreply.github.com>
2020-06-10 21:39:44 +0200

    Update navbar.js

--- static/src/components/navbar.js
+++ static/src/components/navbar.js
@@ -3,40 +3,82 @@ import { Link, Switch, Route } from "react-router-dom";
 import Home from "../pages/home";
 import Impressum from "../pages/impressum";
 import Contact from "../pages/contact";
+import Logout from "../pages/logout";
+import Login from "../pages/login";
 
 class Navbar extends Component {
-    state = {};
-    render() {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <ul className="navbar-nav mr-auto">
-                        <li>
-                            <Link to={"/"} className="nav-link">
-                                {" "}
-                Home{" "}
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={"/contact"} className="nav-link">
-                                Contact
-              </Link>
-                        </li>
-                        <li>
-                            <Link to={"/impressum"} className="nav-link">
-                                About
-              </Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/impressum" component={Impressum} />
-                    <Route path="/contact" component={Contact} />
-                </Switch>
-            </div>
-        );
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  componentWillMount() {
+    if (
+      sessionStorage.getItem("access_token") != null &&
+      sessionStorage.getItem("id_token") != null
+    ) {
+      this.setState({ loggedIn: true });
+    } else {
+      this.setState({ loggedIn: false });
     }
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li>
+              <Link to={"/"} className="nav-link">
+                {" "}
+                Home{" "}
+              </Link>
+            </li>
+            <li>
+              <Link to={"/contact"} className="nav-link">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to={"/impressum"} className="nav-link">
+                About
+              </Link>
+            </li>
+            {this.state.loggedIn == true ? (
+              <li>
+                <Link to={"/logout"} className="nav-link">
+                  {" "}
+                  Logout{" "}
+                </Link>
+              </li>
+            ) : (
+              <li>
+                <Link to={"/login"} className="nav-link">
+                  {" "}
+                  Login{" "}
+                </Link>
+              </li>
+            )}
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route exact path="/" component={Home} />
+          <Route path="/impressum" component={Impressum} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/logout" component={Logout} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
