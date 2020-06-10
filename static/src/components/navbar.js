70212fd9be1aea5b8d232c5860128b1e7d603bb0
BerkanC <31828806+Berkan-C@users.noreply.github.com>
2020-06-10 12:50:00 +0200

    Added new Navbar with logo

--- static/src/components/navbar.js
+++ static/src/components/navbar.js
@@ -3,31 +3,34 @@ import { Link, Switch, Route } from "react-router-dom";
 import Home from "../pages/home";
 import Impressum from "../pages/impressum";
 import Contact from "../pages/contact";
+import logo from '../ressources/navbar-logo.png'
 
 class Navbar extends Component {
     state = {};
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <ul className="navbar-nav mr-auto">
-                        <li>
-                            <Link to={"/"} className="nav-link">
-                                {" "}
-                Home{" "}
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={"/contact"} className="nav-link">
-                                Contact
-              </Link>
-                        </li>
-                        <li>
-                            <Link to={"/impressum"} className="nav-link">
-                                About
-              </Link>
-                        </li>
-                    </ul>
+                <nav className="navbar sticky-top navbar-expand-lg navbar-dark" style={{backgroundColor:"#191919"}}>
+                    <a className ="navbar-nav mr-5 h1" style={{marginTop:"-10px",color:"#efeb53"}}>
+                        <Link to={"/"} className="nav-link">
+                            <img src={logo} height="50px"/>
+                        </Link>
+                    </a>
+                    <a className ="navbar-nav mr-auto" style={{color:"white"}}>
+                        <Link to={"/"} className="nav-link">
+                            Home
+                        </Link>
+                    </a>
+                    <a className ="navbar-nav mr-auto">
+                        <Link to={"/contact"} className="nav-link">
+                            Contact
+                        </Link>
+                        </a>
+                    <a className ="navbar-nav mr-5">
+                        <Link to={"/impressum"} className="nav-link">
+                            About
+                        </Link>
+                    </a>
                 </nav>
                 <Switch>
                     <Route exact path="/" component={Home} />
