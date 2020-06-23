63b4cbebde71b9e9c1603435fd48855a8e425571
BerkanC <31828806+Berkan-C@users.noreply.github.com>
2020-06-23 13:40:59 +0200

    UI for new project creation

--- static/src/pages/newProject.js
+++ static/src/pages/newProject.js
@@ -9,20 +9,20 @@ class newProject extends Component {
     render() {
         return (
         <div className="main createP">
-            
-            <h2 style={{marginLeft:"15px"}}>Create a new Project</h2>
+            <form>
+            <h2 style={{marginLeft:"15px",color:"#efeb53"}}>Create a new Project</h2>
             <div class="input-group">
                 <div class="input-group-prepend">
                     <span class="input-group-text">Title:</span>
                 </div>
-                <input type="text" class="form-control"></input>
+                <input type="text" class="form-control" name="title"></input>
             </div>
 
             <div class="input-group">
                 <div class="input-group-prepend">
                     <span class="input-group-text">Tags:</span>
                 </div>
-                <input type="text" class="form-control"></input>
+                <input type="text" class="form-control" name="tags"></input>
             </div>
             <div style={{color:"grey",marginLeft:"70px",marginTop:"-10px"}}>(divide tags with ",")</div>
             <br/>
@@ -31,10 +31,11 @@ class newProject extends Component {
                 <div class="input-group-prepend">
                     <span class="input-group-text">Description:</span>
                 </div>
-                <textarea class="form-control description"></textarea>
+                <textarea class="form-control description" name="description"></textarea>
             </div>
-            
-            <button class="btn">Create Project</button>
+
+            <button class="btn" type="submit">Create Project</button>
+            </form>
         </div>
         );
     }
