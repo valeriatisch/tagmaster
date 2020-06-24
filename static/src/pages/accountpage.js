02fe4d9b808ff2eeeeacbe30eb9172a830f49ee3
prez567 <64698923+prez567@users.noreply.github.com>
2020-06-24 16:02:06 +0200

    Delete component

--- static/src/pages/accountpage.js
+++ static/src/pages/accountpage.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 
 import Formaccount from "../components/formaccount";
-import Accountheader from "../components/accountheader";
+
 
 
 class Register extends Component {
   render() {
     return (
       <div>
-        <Loginheader/>
-        <Formlogin/>
+        <Formaccount/>
       </div>
     );
   }
