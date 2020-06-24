661218e8403b8f223eb1636e9961c7f2ff7eaa32
Nilinzm <64643592+Nilinzm@users.noreply.github.com>
2020-06-25 01:40:11 +0200

    Update login.js

--- static/src/pages/login.js
+++ static/src/pages/login.js
@@ -1,76 +1,64 @@
-import React, { useState } from "react";
-import { Button, FormGroup, FormControl } from "react-bootstrap";
+import React, { Component } from "react";
+import { Button, FormGroup } from "react-bootstrap";
 import "./login.css";
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
 
-export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    this.state = {
+      email: "",
+      password: "",
+      loginErrors: "",
+    };
 
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   }
 
-  return (
-    <div className="Login">
-      <style>
-        {
-          ".submit-button{ color:#efeb53; background-color:#282828; border: 1px solid #efeb53 } .login-button:hover { background-color:#3F3F3F; color:#efeb53; border: 1px solid }"
-        }
-      </style>
-      <form onSubmit={handleSubmit}>
-        <h4>Sign In</h4>
+  handleSubmit(event) {
+    const { email, password } = this.state;
+  }
 
-        <FormGroup controlId="email" bsSize="large">
-          Email
-          <FormControl
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter Email"
-            border-color="#efeb53"
-            required
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          Password
-          <FormControl
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            placeholder="Enter Password"
-            required
-          />
-        </FormGroup>
-        <div className="form-group">
-          <div className="custom-control custom-checkbox">
+  render() {
+    return (
+      <div className="Login" style={{ color: "white" }}>
+        <form onSubmit={this.handleSubmit}>
+          <h3>Sign In</h3>
+          <FormGroup controlId="email" bsSize="large">
+            Email
             <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              required
             />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
-          </div>
-        </div>
-        <Button
-          class="btn btn-lg submit-button"
-          block
-          bsSize="large"
-          disabled={!validateForm()}
-          type="submit"
-        >
-          Login
-        </Button>
-        <p className="forgot-password text-right">
-          Forgot <a href="#">password?</a>
-        </p>
-      </form>
-    </div>
-  );
+          </FormGroup>
+
+          <FormGroup controlId="password" bsSize="large">
+            Password
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              required
+            />
+          </FormGroup>
+
+          <Button type="submit" block bsSize="large" type="submit">
+            Login
+          </Button>
+        </form>
+      </div>
+    );
+  }
 }
