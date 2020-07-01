ad53dd7e3688e074ef22f5e30730268c43c74844
Nilinzm <64643592+Nilinzm@users.noreply.github.com>
2020-07-01 16:22:15 +0200

    Update App.js

--- static/src/App.js
+++ static/src/App.js
@@ -1,15 +1,64 @@
 // App.js
 import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
+import Home from "./pages/home";
+import Profile from "./pages/profile";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {},
+    };
+
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  checkLoginStatus() {}
+  handleLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {},
+    });
+  }
+
+  handleLogin(data) {
+    this.setState({
+      loggedInStatus: "LOGGED_IN",
+      user: data.user,
+    });
+  }
   render() {
     return (
       <Router>
         <div>
           <Navbar />
         </div>
+        <Switch>
+          <Route
+            exact
+            path={"/home"}
+            render={(props) => (
+              <Home
+                {...props}
+                handleLogin={this.handleLogin}
+                handleLogout={this.handleLogout}
+                loggedInStatus={this.state.loggedInStatus}
+              />
+            )}
+          />
+          <Route
+            exact
+            path={"/profile"}
+            render={(props) => (
+              <Profile {...props} loggedInStatus={this.state.loggedInStatus} />
+            )}
+          />
+        </Switch>
       </Router>
     );
   }
