50b78c38f9d5a1af523d78a2c0fb27deca265bac
Nilinzm <64643592+Nilinzm@users.noreply.github.com>
2020-07-01 16:22:36 +0200

    Update login.js

--- static/src/pages/login.js
+++ static/src/pages/login.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import { Button, FormGroup } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import "./login.css";
+import FacebookLogin from "react-facebook-login";
+import axios from "axios";
+
 export default class Login extends Component {
   constructor(props) {
     super(props);
@@ -14,7 +17,9 @@ export default class Login extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
-
+  responseFacebook(response) {
+    console.log(response);
+  }
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
@@ -23,6 +28,30 @@ export default class Login extends Component {
 
   handleSubmit(event) {
     const { email, password } = this.state;
+    const target = event.target;
+    const name = target.name;
+    axios
+      .post(
+        "http://localhost:3001/api/login	",
+        {
+          user: {
+            email: email,
+            password: password,
+          },
+        },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        console.log("login ");
+
+        // if (response.data.logged_in) {
+        // this.props.handleSuccessfulAuth(response.data);
+        //}
+      })
+      .catch((error) => {
+        console.log("login error");
+      });
+    event.preventDefault();
   }
 
   render() {
@@ -30,7 +59,7 @@ export default class Login extends Component {
       <div className="Login" style={{ color: "white" }}>
         <form onSubmit={this.handleSubmit}>
           <h3>Sign In</h3>
-          <FormGroup controlId="email" bsSize="large">
+          <div className="form-group" controlId="email" bsSize="large">
             Email
             <input
               type="email"
@@ -40,9 +69,8 @@ export default class Login extends Component {
               onChange={this.handleChange}
               required
             />
-          </FormGroup>
-
-          <FormGroup controlId="password" bsSize="large">
+          </div>
+          <div className="form-group" controlId="password" bsSize="large">
             Password
             <input
               type="password"
@@ -52,11 +80,61 @@ export default class Login extends Component {
               onChange={this.handleChange}
               required
             />
-          </FormGroup>
-
-          <Button type="submit" block bsSize="large" type="submit">
-            Login
-          </Button>
+          </div>
+          <div>
+            <div
+              className="custom-control custom-checkbox"
+              style={{
+                zoom: "0.7",
+              }}
+            >
+              <input
+                type="checkbox"
+                className="custom-control-input"
+                id="customCheck1"
+              />
+              <label className="custom-control-label" htmlFor="customCheck1">
+                Remember me
+              </label>
+            </div>
+          </div>
+          <div>
+            <Button
+              className="log-in-button"
+              block
+              type="submit"
+              font-size="20px"
+              style={{
+                margin: "auto",
+                color: "#efeb53",
+                border: "#efeb53",
+                backgroundColor: "#272724f3",
+                width: "20%",
+                fontSize: "1.4vw",
+                height: "1%",
+                marginTop: "5%",
+              }}
+            >
+              Login
+            </Button>
+            <FacebookLogin
+              className="facebook-Button"
+              textButton="login with Facebook"
+              data-layout="default"
+              data-auto-logout-link="true"
+              data-use-continue-as="true"
+              data-width=""
+              appId="1088597931155576"
+              cookie={true}
+              fields="name,email,picture"
+              scope="public_profile,user_friends,user_actions.books"
+              callback={this.responseFacebook}
+              size="small"
+            />
+          </div>
+          <p className="forgot-password">
+            Forgot <a href="#">password?</a>
+          </p>
         </form>
       </div>
     );
