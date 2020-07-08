c9c02ea5139219913ea17446525547d542ad134e
Valeria Tisch <57265123+valeriatisch@users.noreply.github.com>
2020-07-08 19:06:40 +0200

    locust

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/valeriatisch/tagmaster/application"
+	"os"
 )
 
 func main() {
+	_ = os.Setenv("DATABASE_URI", "host=localhost user=valeria dbname=userdb password=password")
+	_ = os.Setenv("SENDGRID_API_KEY", "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")
 	app := application.NewApp()
 	app.Run()
 }
