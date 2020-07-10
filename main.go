109321c23ccc653bc9504916a214f41e748971cd
Foggernaut <49645784+Foggernaut@users.noreply.github.com>
2020-07-10 14:23:00 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 )
 
 func main() {
-	_ = os.Setenv("SENDGRID_API_KEY", "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")
-	_ = os.Setenv("DATABASE_URI", "host=localhost port=5432 user=postgres password=ultrasaur sslmode=disable")
 	app := application.NewApp()
 	app.Run()
 }
