36638e6e11440d77a1c0e1558cc959dd92726599
Nilinzm <64643592+Nilinzm@users.noreply.github.com>
2020-07-11 17:28:26 +0200

    Update login.js

--- static/src/pages/login.js
+++ static/src/pages/login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button, FormGroup, FormControl } from "react-bootstrap";
 import "./login.css";
+import FacebookLogin from "react-facebook-login";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -15,7 +16,7 @@ export default function Login() {
   }
 
   return (
-    <div className="Login" style={{color:"white"}}>
+    <div className="Login" style={{ color: "white" }}>
       <form onSubmit={handleSubmit}>
         <h3>Sign In</h3>
 
@@ -48,9 +49,39 @@ export default function Login() {
             </label>
           </div>
         </div>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+        <Button
+          block
+          disabled={!validateForm()}
+          className="log-in-button"
+          block
+          type="submit"
+          font-size="20px"
+          style={{
+            margin: "auto",
+            color: "#efeb53",
+            backgroundColor: "#272724f3",
+            width: "20%",
+            fontSize: "1.4vw",
+            height: "1%",
+            marginTop: "5%",
+          }}
+          type="submit"
+        >
           Login
         </Button>
+        <FacebookLogin
+          className="facebook-Button"
+          textButton="login with Facebook"
+          data-layout="default"
+          data-auto-logout-link="true"
+          data-use-continue-as="true"
+          data-width=""
+          appId="1088597931155576"
+          cookie={true}
+          fields="name,email,picture"
+          scope="public_profile,user_friends,user_actions.books"
+          size="small"
+        />
         <p className="forgot-password text-right">
           Forgot <a href="#">password?</a>
         </p>
