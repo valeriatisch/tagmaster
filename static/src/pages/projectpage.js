92406d6fcfb3d47be5bd47097c0846eddcd48f4b
TheoHtu <65022251+TheoHtu@users.noreply.github.com>
2020-07-24 15:13:34 +0200

    Frontend Pages adjusted to fit with Backend

--- static/src/pages/projectpage.js
+++ static/src/pages/projectpage.js
@@ -7,7 +7,7 @@ class Register extends Component {
     render() {
         return (
             <>
-                <Project title="Animals" description="Label different animals from your everyday pets to exotic wildlife." tags={["dog","cat","bird","snake","lion","seal","bug","spider","fish"]}/>
+                <Project title="Animals" tags={["dog","cat","bird","snake","lion","seal","bug","spider","fish"]}/>
             </>
         );
      }
