77c7433c7eef9d95f6a2caa89148f4f3f4e229b1
Nilinzm <64643592+Nilinzm@users.noreply.github.com>
2020-07-25 11:03:14 +0200

    Update impressum.js

--- static/src/pages/impressum.js
+++ static/src/pages/impressum.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
 class Impressum extends Component {
-    render() {
-        return (
-            <div>
-                <h2 style={{color:"white"}}>Impressum</h2>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2 style={{ color: "white" }}>Impressum</h2>
+      </div>
+    );
+  }
 }
 
 export default Impressum;
