d6999d758babb52aa326d09da6e8b3894c1ed424
Nilinzm <64643592+Nilinzm@users.noreply.github.com>
2020-07-25 11:02:19 +0200

    Update navbar.js

--- static/src/components/navbar.js
+++ static/src/components/navbar.js
@@ -1,17 +1,21 @@
-import React, { Component } from "react";
-import { Link, Switch, Route } from "react-router-dom";
-import Home from "../pages/home";
-import Impressum from "../pages/impressum";
-import Contact from "../pages/contact";
-import Logout from "../pages/logout";
-import Login from "../pages/login";
-import newProject from "../pages/newProject";
-import logo from "../ressources/navbar-logo.png";
-import Projects from "../pages/projects.js";
-import Accountpage from "../pages/accountpage";
-import ProfileArea from "../pages/ProfileArea";
+import React, { Component } from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
+import Home from '../pages/home';
+import Impressum from '../pages/impressum';
+import Contact from '../pages/contact';
+import Logout from '../pages/logout';
+import Login from '../pages/login';
+import newProject from '../pages/newProject';
+import logo from '../ressources/navbar-logo.png';
+import Projects from '../pages/projects.js';
+import Accountpage from '../pages/accountpage';
+import Profile from '../pages/profile';
+import { Nav, Navbar } from 'react-bootstrap';
+import Label from '../pages/label';
+import ProjectPage from '../pages/projectpage';
+import { auth } from '../firebase/firebase';
 
-class Navbar extends Component {
+class ownNavbar extends Component {
   constructor(props) {
     super(props);
     this.toggle = this.toggle.bind(this);
@@ -20,15 +24,20 @@ class Navbar extends Component {
     };
   }
 
-  componentWillMount() {
-    if (
-      sessionStorage.getItem("access_token") != null &&
-      sessionStorage.getItem("id_token") != null
-    ) {
-      this.setState({ loggedIn: true });
-    } else {
-      this.setState({ loggedIn: false });
-    }
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      console.log('Got New User', user);
+      window.user = user;
+      if (
+        (sessionStorage.getItem('access_token') != null &&
+          sessionStorage.getItem('id_token') != null) ||
+        window.user
+      ) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
 
   toggle() {
@@ -39,115 +48,97 @@ class Navbar extends Component {
 
   render() {
     return (
-      <div
-        className="container col-auto"
-        style={{ padding: "0px", backgroundColor: "#191919" }}
-      >
-        <style>
-          {"a { color: #efeb53; fontSize:15px } a:hover{ color:white }"}
-        </style>
+      <>
         <style>
           {
-            ".login-button {width: 100%; color:#efeb53; background-color:#282828; border: 1px solid #efeb53 } .login-button:hover { background-color:#3F3F3F; color:#efeb53; border: 1px solid }"
+            '.login-button{width:150px;border:1px solid #efeb53;text-align:center;background-color:#282828;color:#efeb53} .login-button:hover{background-color:#3F3F3F;color:#efeb53}'
           }
         </style>
-        <style>
-          {
-            ".logout-button{width: 100%; color:#efeb53; background-color:#282828; border: 1px solid #efeb53 } .logout-button:hover { background-color:#3F3F3F; color:#efeb53; border: 1px solid }"
-          }
-        </style>
-        <nav
-          className="navbar sticky-top navbar-expand-lg"
+        <Navbar
+          expand='md'
           style={{
-            backgroundColor: "#191919",
-            borderBottom: "2px solid #2e2d2d",
-          }}
-        >
-          <a
-            className="nav-link mr-3 h1"
-            style={{ marginTop: "-20px", marginBottom: "-10px" }}
-          >
-            <Link to={"/"} className="nav-link">
-              <img src={logo} height="45px" />
-            </Link>
-          </a>
-          <div className="navbar-nav mr-auto">
-            <a className="mr-auto">
-              <Link to={"/ProfileArea"} className="nav-link">
-                Profile
-              </Link>
-            </a>
-
-            <a className="mr-3">
-              <Link to={"/"} className="nav-link">
+            backgroundColor: '#191919',
+            borderBottom: '1px solid #454545',
+          }}>
+          <Navbar.Brand href='/' style={{ marginTop: '-5px' }}>
+            <img src={logo} height='50' />
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls='basic-navbar-nav'
+            style={{
+              width: '70px',
+              borderColor: '#efeb53',
+              backgroundColor: '#3F3F3F',
+            }}
+          />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='mr-auto' style={{}}>
+              <Nav.Link href='/' style={{ color: '#efeb53' }}>
                 Home
-              </Link>
-            </a>
-
-            <a className="mr-3">
-              <Link to={"/projects"} className="nav-link">
+              </Nav.Link>
+              <Nav.Link href='/projects' style={{ color: '#efeb53' }}>
                 Projects
-              </Link>
-            </a>
-            <a className="mr-3">
-              <Link to={"/contact"} className="nav-link">
+              </Nav.Link>
+              <Nav.Link href='/contact' style={{ color: '#efeb53' }}>
                 Contact
-              </Link>
-            </a>
-            <a className="mr-auto">
-              <Link to={"/impressum"} className="nav-link">
+              </Nav.Link>
+              <Nav.Link href='/impressum' style={{ color: '#efeb53' }}>
                 About
-              </Link>
-            </a>
-          </div>
-          <div className="navbar-nav navbar-right">
-            <a className="mr-3">
+              </Nav.Link>
+            </Nav>
+            <Nav className='navbar-right '>
               {this.state.loggedIn == true ? (
-                <Link to={"/logout"} className="nav-link">
-                  {" "}
-                  Logout{" "}
-                </Link>
+                <>
+                  <div className='text-light mt-2'>
+                    Email : {auth.currentUser.email}
+                  </div>
+                  <Link
+                    to={'/logout'}
+                    className='nav-link'
+                    onClick={() => {
+                      auth
+                        .signOut()
+                        .then((res) => {
+                          console.log(res);
+                        })
+                        .catch((err) => {
+                          console.log(err.message);
+                        });
+                    }}>
+                    <div className='text-light'>Logout</div>
+                  </Link>
+                </>
               ) : (
-                <Link to={"/login"} className="nav-link">
+                <Link to={'/login'}>
                   <button
-                    type="button"
-                    class="btn btn-lg login-button"
-                    role="button"
-                    aria-pressed="true"
-                  >
-                    Login
+                    type='button'
+                    class='btn btn-lg login-button'
+                    role='button'
+                    aria-pressed='true'>
+                    Log in
                   </button>
                 </Link>
               )}
-            </a>
-          </div>
-        </nav>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
         <Switch>
-          <Route path="/login" component={Login} />
-          <Route exact path="/" component={Home} />
-          <Route path="/impressum" component={Impressum} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/user/newProject" component={newProject} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/accountpage" component={Accountpage} />
+          <Route path='/login' component={Login} />
+          <Route exact path='/' component={Home} />
+          <Route path='/impressum' component={Impressum} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/logout' component={Logout} />
+          <Route path='/user/newProject' component={newProject} />
+          <Route path='/projects' component={Projects} />
+          <Route path='/accountpage' component={Accountpage} />
+          <Route path='/label' component={Label} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/user/project' component={ProjectPage} />
         </Switch>
-      </div>
+      </>
     );
   }
 }
 
-export default Navbar;
-/*{this.state.loggedIn == true ? (
-  <a className="mr-auto">
-  <Link to={"/ProfileArea"} className="nav-link">
-    Profile
-  </Link>
-</a>
-) : (
-<a className="mr-3">
-  <Link to={"/"} className="nav-link">
-    Home
-  </Link>
-</a>
-)}*/
+export default ownNavbar;
