26eb23dcb162205352cf870034dde2dfb8616b08
Nilinzm <64643592+Nilinzm@users.noreply.github.com>
2020-07-25 11:00:42 +0200

    Update App.js

--- static/src/App.js
+++ static/src/App.js
@@ -1,36 +1,11 @@
 // App.js
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
-import Home from "./pages/home";
-import ProfileArea from "./pages/ProfileArea";
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/navbar';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      loggedInStatus: "NOT_LOGGED_IN",
-      user: {},
-    };
-
-    this.handleLogin = this.handleLogin.bind(this);
-    this.handleLogout = this.handleLogout.bind(this);
-  }
-
-  checkLoginStatus() {}
-  handleLogout() {
-    this.setState({
-      loggedInStatus: "NOT_LOGGED_IN",
-      user: {},
-    });
-  }
-
-  handleLogin(data) {
-    this.setState({
-      loggedInStatus: "LOGGED_IN",
-      user: data.user,
-    });
+  componentDidMount() {
+    console.log('gg', window.user);
   }
   render() {
     return (
@@ -38,31 +13,6 @@ class App extends Component {
         <div>
           <Navbar />
         </div>
-
-        <Switch>
-          <Route
-            exact
-            path={"/home"}
-            render={(props) => (
-              <Home
-                {...props}
-                handleLogin={this.handleLogin}
-                handleLogout={this.handleLogout}
-                loggedInStatus={this.state.loggedInStatus}
-              />
-            )}
-          />
-          <Route
-            exact
-            path={"/ProfileArea"}
-            render={(props) => (
-              <ProfileArea
-                {...props}
-                loggedInStatus={this.state.loggedInStatus}
-              />
-            )}
-          />
-        </Switch>
       </Router>
     );
   }
