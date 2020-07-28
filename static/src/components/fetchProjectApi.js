8d711c8f68b03c1cea0460e98c4d39865cd2de80
Riccardo <l.riccardo@web.de>
2020-07-29 00:34:18 +0200

    fix project overview bugs

--- static/src/components/fetchProjectApi.js
+++ static/src/components/fetchProjectApi.js
@@ -86,6 +86,20 @@ export const createProject = async (params) => {
     });
 };
 
+export const getProjectDetails = async (url) => {
+  return fetch(url, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      /* console.log("Success:", data); */
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
 
 // export const createProject = async (params) => {
 //   return fetch("/api/projects/", {
