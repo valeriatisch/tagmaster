4e5df0efd12d59c7a8334e33b2926674ca9aa5dc
Riccardo <l.riccardo@web.de>
2020-07-28 20:31:08 +0200

    project create fix

--- static/src/components/fetchProjectApi.js
+++ static/src/components/fetchProjectApi.js
@@ -71,6 +71,22 @@ export const usePicture = (url) => {
   return state;
 };
 
+export const createProject = async (params) => {
+  return fetch("/api/projects/", {
+    method: "POST",
+    body: JSON.stringify(params),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+
 // export const createProject = async (params) => {
 //   return fetch("/api/projects/", {
 //     method: "POST",
