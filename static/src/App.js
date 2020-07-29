2e245bf219acc7a13dd8599c42c022eccfb407c3
Riccardo <l.riccardo@web.de>
2020-07-29 18:29:43 +0200

    added footer to appjs

--- static/src/App.js
+++ static/src/App.js
@@ -39,7 +39,7 @@ export default function App() {
 
             <Navbar auth={handleAuth} authToken={auth} />
             <Routing />
-            {/*  <Footer /> */}
+            <Footer />
           </div>
         </Router>
       </AuthApi.Provider>
